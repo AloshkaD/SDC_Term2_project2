471bea067f184a4da54a3371a75f26c9f0b784d6
AloshkaD <tew142@hotmail.com>
2017-04-08 19:36:21 -0400

    Added RMSE formula

--- src/tools.cpp
+++ src/tools.cpp
@@ -14,5 +14,17 @@ VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
   /**
   TODO:
     * Calculate the RMSE here.
+
   */
+    VectorXd rmse(4);
+    rmse << 0,0,0,0;
+    if (estimations.size() == 0 || estimations.size() != ground_truth.size()) 
+      {
+        
+        cout<<"Invalid ground trusth or estimation data input for RMSE"<<"\n";
+
+        return rmse;
+          
+      }
+
 }
